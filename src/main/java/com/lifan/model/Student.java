6d263eb3d8d967bef9ab9a43c207b4ea561649af
李帆 <1783670283@qq.com>
2021-01-17 17:30:18 +0800

    Initial commit

---
+++ src/main/java/com/lifan/model/Student.java
@@ -0,0 +1,14 @@
+package com.lifan.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class Student implements Serializable {
+    private Integer id;
+    private String name;
+    private Double score;
+    private Date birthday;
+}
